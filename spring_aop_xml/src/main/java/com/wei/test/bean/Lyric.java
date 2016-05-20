dc90722e306c7ba5f90373740c619b652016e362
Wei <userchenwei@gmail.com>
2016-05-21 00:05:04 +0800

    spring aop call new interface

---
+++ spring_aop_xml/src/main/java/com/wei/test/bean/Lyric.java
@@ -0,0 +1,8 @@
+package com.wei.test.bean;
+
+/**
+ * Created by cw on 5/20/16.
+ */
+public interface Lyric {
+  void show();
+}
---
+++ spring_aop_xml/src/main/java/com/wei/test/bean/impl/Song.java
@@ -0,0 +1,14 @@
+package com.wei.test.bean.impl;
+
+import com.wei.test.bean.Lyric;
+
+/**
+ * Created by cw on 5/20/16.
+ */
+public class Song implements Lyric {
+
+  public void show() {
+    System.out.println("This is lyric in the song");
+  }
+
+}
