f4dd87b1d32ad371c0358d8accbd0c2f188921b5
wei <userchenwei@gmail.com>
2016-06-01 17:53:44 +0800

    hello freemarker

---
+++ springMVC_with_freeMarker/src/main/java/com/wei/test/data/User.java
@@ -0,0 +1,36 @@
+package com.wei.test.data;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by cw on 6/1/16.
+ */
+public class User {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String age;
+
+    public User(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
