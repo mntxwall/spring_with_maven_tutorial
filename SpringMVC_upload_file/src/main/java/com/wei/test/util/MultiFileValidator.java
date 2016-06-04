1820fc9e36bb6bd6ea5d450bce2c754a943a3d5c
Wei <userchenwei@gmail.com>
2016-06-05 00:13:49 +0800

    ????

---
+++ SpringMVC_upload_file/src/main/java/com/wei/test/util/MultiFileValidator.java
@@ -0,0 +1,32 @@
+package com.wei.test.util;
+
+import com.wei.test.model.FileBucket;
+import com.wei.test.model.MultiFileBucket;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+
+@Component
+public class MultiFileValidator implements Validator {
+	
+	public boolean supports(Class<?> clazz) {
+		return MultiFileBucket.class.isAssignableFrom(clazz);
+	}
+
+	public void validate(Object obj, Errors errors) {
+		MultiFileBucket multiBucket = (MultiFileBucket) obj;
+		
+		int index=0;
+		
+		for(FileBucket file : multiBucket.getFiles()){
+			if(file.getFile()!=null){
+				if (file.getFile().getSize() == 0) {
+					errors.rejectValue("files["+index+"].file", "missing.file");
+				}
+			}
+			index++;
+		}
+		
+	}
+}
--- springMVC_tutorial_annotation/src/main/java/com/wei/test/web/HelloController.java
+++ springMVC_tutorial_annotation/src/main/java/com/wei/test/web/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Created by cw on 5/27/16.
  */
 @Controller
-@RequestMapping(name = "/home")
+@RequestMapping(value = "/home")
 public class HelloController {
   @RequestMapping(method = RequestMethod.GET)
   public String hello(){
