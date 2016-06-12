fea66c67595823870da1310c1acef7333848b2e6
wei <userchenwei@gmail.com>
2016-06-12 17:30:13 +0800

    new learning project

---
+++ FlowEdit/src/main/java/com/wei/project/config/FlowEditWebApp.java
@@ -0,0 +1,20 @@
+package com.wei.project.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by cw on 6/12/16.
+ */
+public class FlowEditWebApp extends AbstractAnnotationConfigDispatcherServletInitializer{
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] {WebConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
---
+++ FlowEdit/src/main/java/com/wei/project/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.wei.project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by cw on 6/12/16.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter{
+}
